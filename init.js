72fefa818c0908c329ddfc71f5e9f976e447e531
sqfasd <sqf1225@foxmail.com>
2017-09-24 15:48:08 +0800

    remove newBlock event handler

--- init.js
+++ init.js
@@ -8,12 +8,8 @@ module.exports = async function () {
   app.registerContract(1002, 'cctime.voteArticle')
   app.registerContract(1003, 'cctime.likeComment')
   app.registerContract(1004, 'cctime.report')
-  
+
   app.setDefaultFee('10000000', 'CCTime.XCT')
 
   app.custom.cache = new IntervalCache(10 * 1000)
-
-  app.events.on('newBlock', (block) => {
-    app.logger.info('new block received', block.height)
-  })
 }
